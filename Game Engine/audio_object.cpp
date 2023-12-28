a9e95adec8247c958020a73036117499d8ef5cf2
Riccardo Damiani <riccardodamiani.28@gmail.com>
2023-12-28 17:39:57 +0100

    added spatial sound to audio object

--- Game Engine/audio_object.cpp
+++ Game Engine/audio_object.cpp
@@ -4,8 +4,11 @@
 
 unsigned long AudioObj::_tracksCounter = 0;
 
-AudioObj::AudioObj(std::string trackName) : 
-	_name(DecodeName(trackName.c_str()))
+AudioObj::AudioObj(std::string trackName, bool spatial_sound, double maxDistance, vector2 source_position) :
+	_name(DecodeName(trackName.c_str())),
+	_spatial_sound(spatial_sound),
+	_maxDistance(maxDistance),
+	_src_pos(source_position)
 {
 	_len = 0;
 	_tracksCounter++;
@@ -13,22 +16,27 @@ AudioObj::AudioObj(std::string trackName) :
 	_status = AudioStatus::AUDIO_STATUS_IDLE;
 }
 
+//return a unique id for the current track
 unsigned long AudioObj::getTrackId() {
 	return _trackId;
 }
 
+//get length in seconds of the current track
 bool AudioObj::GetLen() {
 	return _len;
 }
 
+//pause the track
 void AudioObj::Pause() {
 	_AudioEngine->PauseAudioObject(shared_from_this());
 }
 
+//stops the track
 void AudioObj::Stop() {
 	_AudioEngine->StopAudioObject(shared_from_this());
 }
 
+//resume a paused track
 void AudioObj::Resume() {
 	_AudioEngine->ResumeAudioObject(shared_from_this());
 }
@@ -64,8 +72,8 @@ void AudioObj::__setChannel(int channel) {
 
 
 
-DialogAudioObj::DialogAudioObj(std::string trackName) :
-	AudioObj(trackName)
+DialogAudioObj::DialogAudioObj(std::string trackName, bool spatial_sound, double maxDistance, vector2 source_position) :
+	AudioObj(trackName, spatial_sound, maxDistance, source_position)
 {
 	_len = _AudioEngine->GetDialogLen(_name);
 }
@@ -74,14 +82,14 @@ bool DialogAudioObj::Play() {
 	if (GetStatus() != AUDIO_STATUS_IDLE) {
 		return false;
 	}
-	_AudioEngine->PlayDialog(_name, shared_from_this());
+	_AudioEngine->PlayDialog(_name, shared_from_this(), _spatial_sound, _maxDistance, _src_pos);
 }
 
 
 
 
-EffectAudioObj::EffectAudioObj(std::string trackName) :
-	AudioObj(trackName)
+EffectAudioObj::EffectAudioObj(std::string trackName, bool spatial_sound, double maxDistance, vector2 source_position) :
+	AudioObj(trackName, spatial_sound, maxDistance, source_position)
 {
 	_len = _AudioEngine->GetSoundEffectLen(_name);
 }
@@ -90,14 +98,14 @@ bool EffectAudioObj::Play() {
 	if (GetStatus() != AUDIO_STATUS_IDLE) {
 		return false;
 	}
-	_AudioEngine->PlaySoundEffect(_name, shared_from_this());
+	_AudioEngine->PlaySoundEffect(_name, shared_from_this(), _spatial_sound, _maxDistance, _src_pos);
 }
 
 
 
 
-UIAudioObj::UIAudioObj(std::string trackName) :
-	AudioObj(trackName)
+UIAudioObj::UIAudioObj(std::string trackName, bool spatial_sound, double maxDistance, vector2 source_position) :
+	AudioObj(trackName, spatial_sound, maxDistance, source_position)
 {
 	_len = _AudioEngine->GetUIEffectLen(_name);
 }
@@ -106,5 +114,5 @@ bool UIAudioObj::Play() {
 	if (GetStatus() != AUDIO_STATUS_IDLE) {
 		return false;
 	}
-	_AudioEngine->PlayUIEffect(_name, shared_from_this());
+	_AudioEngine->PlayUIEffect(_name, shared_from_this(), _spatial_sound, _maxDistance, _src_pos);
 }
--- Game Engine/audio_object.h
+++ Game Engine/audio_object.h
@@ -19,7 +19,7 @@ enum AudioStatus {
 
 class AudioObj : public std::enable_shared_from_this<AudioObj> {
 public:
-	AudioObj(std::string trackName);
+	AudioObj(std::string trackName, bool spatial_sound = false, double maxDistance = 0, vector2 source_position = { 0, 0 });
 	virtual bool Play() = 0;
 	bool GetLen();
 	void Pause();
@@ -35,17 +35,21 @@ public:
 protected:
 	const EntityName _name;
 	double _len;
+	const bool _spatial_sound;
+	const double _maxDistance;
+	const vector2 _src_pos;
 private:
 	std::atomic <int> _channel = -1;	//channel where the track is being played or was played
 	std::atomic <AudioStatus> _status;	//track status
 	unsigned long _trackId;	//unique id for the current track
 	static unsigned long _tracksCounter;	//keeps track of the total number of tracks objects created
+
 };
 
 
 class DialogAudioObj : public AudioObj {
 public:
-	DialogAudioObj(std::string trackName);
+	DialogAudioObj(std::string trackName, bool spatial_sound = false, double maxDistance = 0, vector2 source_position = { 0, 0 });
 	virtual bool Play();
 private:
 };
@@ -53,7 +57,7 @@ private:
 
 class EffectAudioObj : public AudioObj {
 public:
-	EffectAudioObj(std::string trackName);
+	EffectAudioObj(std::string trackName, bool spatial_sound = false, double maxDistance = 0, vector2 source_position = { 0, 0 });
 	virtual bool Play();
 private:
 };
@@ -61,7 +65,7 @@ private:
 
 class UIAudioObj : public AudioObj {
 public:
-	UIAudioObj(std::string trackName);
+	UIAudioObj(std::string trackName, bool spatial_sound = false, double maxDistance = 0, vector2 source_position = { 0, 0 });
 	virtual bool Play();
 private:
 };
