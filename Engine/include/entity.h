500d34c014a082b57a9f5c1a1b9477c997fcd0b4
Riccardo Damiani <riccardodamiani.28@gmail.com>
2024-07-21 21:57:04 +0200

    fixed linking issue for DecodeName function

--- Engine/include/entity.h
+++ Engine/include/entity.h
@@ -1,8 +1,37 @@
 #ifndef ENTITY_H
 #define ENTITY_H
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
 typedef unsigned long long EntityName;
+constexpr const EntityName DecodeName(const char* name) {
+	//FNV-1a hashing algorithm
+	int len = 0;
+	//basic strlen
+	for (int i = 0; i < 100; i++) {
+		if (name[i] == 0) {
+			len = i;
+			break;
+		}
+	}
+	if (len == 0)
+		return 0;
+	EntityName hash = 14695981039346656037;
+	EntityName FNV_prime = 1099511628211;
 
-ENGINE_API constexpr const EntityName DecodeName(const char* name);
+	for (int i = 0; i < len; i++) {
+		hash ^= name[i];
+		hash *= FNV_prime;
+	}
+
+	return hash;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif
